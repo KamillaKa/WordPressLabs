74d722e132f454043d1c9161fe16f364415ab2f3
Kamilla <KamillaKa@users.noreply.github.com>
2024-03-26 13:42:24 +0200

    cleaned code and made article-function

--- wp-content/themes/example/header.php
+++ wp-content/themes/example/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
@@ -14,6 +14,6 @@
             <?php the_custom_logo(); ?>
         </div>
         <div class="header-top-right">
-            <?php wp_nav_menu( [ 'container' => 'nav', 'theme_location' => 'main-menu' ] ); ?>
+            <?php wp_nav_menu(['container' => 'nav', 'theme_location' => 'main-menu']); ?>
         </div>
     </header>
